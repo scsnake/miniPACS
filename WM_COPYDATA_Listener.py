2ae4b78adf3c707333e0d6bc96dcaea6b25fdcb7
scsnake <scsnake@gmail.com>
2017-08-17 22:49:14 +0800

    Create WM_COPYDATA_Listener.py

---
+++ WM_COPYDATA_Listener.py
@@ -0,0 +1,60 @@
+import win32con, win32api, win32gui, ctypes, ctypes.wintypes
+import threading
+
+
+class COPYDATASTRUCT(ctypes.Structure):
+    _fields_ = [
+        ('dwData', ctypes.wintypes.LPARAM),
+        ('cbData', ctypes.wintypes.DWORD),
+        ('lpData', ctypes.c_void_p)
+    ]
+
+
+PCOPYDATASTRUCT = ctypes.POINTER(COPYDATASTRUCT)
+
+
+class WM_COPYDATA_Listener:
+    def __init__(self):
+        message_map = {
+            win32con.WM_COPYDATA: self.__OnCopyData
+        }
+        wc = win32gui.WNDCLASS()
+        wc.lpfnWndProc = message_map
+        wc.lpszClassName = 'MyWindowClass'
+        hinst = wc.hInstance = win32api.GetModuleHandle(None)
+        classAtom = win32gui.RegisterClass(wc)
+        self.hwnd = win32gui.CreateWindow(
+            classAtom,
+            "WM_COPYDATA_Listener",
+            0,
+            0,
+            0,
+            win32con.CW_USEDEFAULT,
+            win32con.CW_USEDEFAULT,
+            0,
+            0,
+            hinst,
+            None
+        )
+        # print self.hwnd
+
+    def OnCopyData(self, *args, **kwargs):
+        for k in ['kwnd', 'msg', 'wparam', 'lparam', 'dwData', 'cbData', 'lpData']:
+            if k in kwargs:
+                print kwargs[k] if k!='lpData' else ctypes.wstring_at(kwargs[k])
+
+    def __OnCopyData(self, hwnd, msg, wparam, lparam):
+        pCDS = ctypes.cast(lparam, PCOPYDATASTRUCT)
+
+        t = threading.Thread(target=self.OnCopyData,
+                             kwargs={'hwnd':hwnd, 'msg':msg,
+                                     'wparam':wparam, 'lparam':lparam,
+                                     'dwData':pCDS.contents.dwData,
+                                     'cbData':pCDS.contents.cbData,
+                                     'lpData':pCDS.contents.lpData})
+        t.start()
+
+        return 1
+
+COPYDATA_Listener = WM_COPYDATA_Listener()
+win32gui.PumpMessages()
